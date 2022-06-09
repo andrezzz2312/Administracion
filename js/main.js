3eaa1be1c6e8632148b10f2744ae9c7ea5114852
Andrez Ramirez <andrezzz2312@gmail.com>
2022-06-09 10:27:54 -0500

    restablish window

--- js/main.js
+++ js/main.js
@@ -20,10 +20,10 @@ $(function () {
 const toggle = document.querySelector('.toggle')
 const modal = document.querySelectorAll('.modal')
 let counter = 0
-modal.forEach((e, f) => {
-  modal[f].style.display = 'none'
-  modal[2].style.display = 'block'
-})
+// modal.forEach((e, f) => {
+//   modal[f].style.display = 'none'
+//   modal[2].style.display = 'block'
+// })
 toggle.addEventListener('click', function (e) {
   counter = counter + 1
   modal.forEach((e, f) => {
