87093634bec6eb0848493661571369e368f89296
Andrez Ramirez <andrezzz2312@gmail.com>
2022-06-10 12:13:09 -0500

    final

--- js/main.js
+++ js/main.js
@@ -17,38 +17,32 @@ $(function () {
   $('#tree').fancytree()
 })
 
-const toggle = document.querySelector('.toggle')
+const bodegas = document.querySelector('#bodegas')
 const modal = document.querySelectorAll('.modal')
 let counter = 0
 // modal.forEach((e, f) => {
 //   modal[f].style.display = 'none'
 //   modal[2].style.display = 'block'
 // })
-toggle.addEventListener('click', function (e) {
-  counter = counter + 1
-  modal.forEach((e, f) => {
-    modal[f].style.display = 'none'
-    console.log(e)
-    console.log(counter)
-  })
+const fileManager = document.querySelector('.fileManager')
 
-  modal[counter].style.display = 'block'
-
-  if (counter === 2) {
-    counter = -1
-  }
+bodegas.addEventListener('click', function () {
+  console.log('asd')
+  console.log(fileManager)
+  fileManager.style.display = 'block'
 })
+
 $('#tree').fancytree({
   dblclick: function (event, data) {
     // A node was activated: display its title:
     var keyNode = data.node.key
-    if (keyNode === 'fer') {
+    if (keyNode === 're') {
       modal.forEach((e, f) => {
         modal[f].style.display = 'none'
         modal[0].style.display = 'block'
       })
     }
-    if (keyNode === 're') {
+    if (keyNode === 'fer') {
       modal.forEach((e, f) => {
         modal[f].style.display = 'none'
         modal[1].style.display = 'block'
@@ -56,18 +50,39 @@ $('#tree').fancytree({
     }
   },
 })
-// $('#tree').fancytree({
-//   dblclick: function (event, data) {
-//     console.log('epic')
-//     //				data.node.toggleSelect();
-//   },
-// })
 
-// const op4 = document.querySelector('#op4')
-// op4.addEventListener('fancytreeclick', function (e) {
-//   console.log('asd')
-//   modal.forEach((e, f) => {
-//     modal[f].style.display = 'none'
-//     modal[0].style.display = 'block'
-//   })
-// })
+const emitir = document.querySelector('#emitir')
+const alert = document.querySelector('.alert')
+const x = document.querySelector('.alert_x')
+const si = document.querySelector('#si')
+const no = document.querySelector('#no')
+
+emitir.addEventListener('click', function () {
+  alert.style.display = 'block'
+  si.addEventListener('click', function () {
+    alert.style.display = 'none'
+    emitir.setAttribute('disabled', '')
+  })
+  no.addEventListener('click', function () {
+    alert.style.display = 'none'
+  })
+  x.addEventListener('click', function () {
+    alert.style.display = 'none'
+  })
+})
+
+$(function () {
+  $('.alert').draggable()
+})
+$(function () {
+  $('.modal').draggable({
+    containment: 'parent',
+  })
+})
+
+const closet = document.querySelectorAll('#closet')
+closet.forEach((e) => {
+  e.addEventListener('click', function (e) {
+    e.target.parentElement.parentElement.parentElement.style.display = 'none'
+  })
+})
